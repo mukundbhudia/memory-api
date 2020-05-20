5bb7e766ebf47322536c45b522cfa2320025ca8b
Mukund Bhudia <mukund_bhudia@hotmail.com>
2020-05-20 16:38:34 +0100

    Added signup route and body parser to process data

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express()
 const PORT = process.env.PORT || 4000
@@ -7,10 +8,20 @@ const { connectDB, getDBClient, disconnectDB } = require('./dbClient.js')
 
 require('dotenv').config()
 
+const options = {
+  inflate: true,
+}
+
 const startServer = async () => {
     await connectDB()
     app.use(cors())
+    app.use(bodyParser.json(options))
     app.get('/', (req, res) => res.json({ msg: 'Welcome to the memory API!' }))
+    app.post('/signup', (req, res) => {
+      console.log(req.body)
+      
+      res.json({ msg: 'posted' })
+    })
     app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`))
   }
   
